0e97c0d77160af51c60870ce6cdd5413df0cad60
JSPRO-11(Subham Saha) <js-pro-11@JSPRO-11s-MacBook-Pro.local>
2022-12-28 17:19:55 +0530

    ehundi completed

--- pages/e-hundi.vue
+++ pages/e-hundi.vue
@@ -9,108 +9,160 @@
                         <div class="heading">
                             <h4 class="lower-heading text-unset">e-Hundi</h4>
                         </div>
-                        <form id="hundiForm" action="https://hrudayaspandana.org/e-hundi" method="post">
-                            <input autocomplete="off" type="hidden" name="ci_csrf_token" value="">
+                        <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+                        <form id="hundiForm" method="post" @submit.prevent="handleSubmit(formHandler)">
                             <div class="row">
                                 <div class="col-lg-6">
                                     <div class="mb-3">
-                                        <input type="text" id="fname" name="fname" class="form-control form-hundi-input"
-                                            placeholder="First Name*" value="">
-                                        <div style="color:red;"></div>
+                                        <ValidationProvider v-slot="{ classes, errors }" rules="required|alpha_spaces" name="first name">
+                                            <input 
+                                                id="fname" 
+                                                v-model="first_name" 
+                                                type="text" 
+                                                name="fname" 
+                                                class="form-control form-hundi-input"
+                                                placeholder="First Name" value="">
+                                            <div :class="classes">{{ errors[0] }}</div>
+                                        </ValidationProvider>
                                     </div>
                                 </div>
                                 <div class="col-lg-6">
                                     <div class="mb-3">
-                                        <input type="text" id="lname" name="lname" class="form-control form-hundi-input"
-                                            placeholder="Last Name*" value="">
-                                        <div style="color:red;"></div>
+                                        <ValidationProvider v-slot="{ classes, errors }" rules="required|alpha_spaces" name="last name">
+                                            <input 
+                                                id="lname" 
+                                                v-model="last_name" 
+                                                type="text" 
+                                                name="lname" 
+                                                class="form-control form-hundi-input"
+                                                placeholder="Last Name" value="">
+                                            <div :class="classes">{{ errors[0] }}</div>
+                                        </ValidationProvider>
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-lg-6">
                                     <div class="mb-3">
-                                        <input type="text" id="phone" name="phone" class="form-control form-hundi-input"
-                                            placeholder="Phone Number*" value="">
-                                        <div style="color:red;"></div>
+                                        <ValidationProvider v-slot="{ classes, errors }" rules="required|phone" name="phone">
+                                            <input 
+                                                id="phone" 
+                                                v-model="phone" 
+                                                type="text" 
+                                                name="phone" 
+                                                class="form-control form-hundi-input"
+                                                placeholder="Phone Number" value="">
+                                            <div :class="classes">{{ errors[0] }}</div>
+                                        </ValidationProvider>
                                     </div>
                                 </div>
                                 <div class="col-lg-6">
                                     <div class="mb-3">
-                                        <input type="email" id="email" name="email"
-                                            class="form-control form-hundi-input" placeholder="Email*" value="">
-                                        <div style="color:red;"></div>
+                                        <ValidationProvider v-slot="{ classes, errors }" rules="required|email" name="email">
+                                            <input 
+                                                id="email" 
+                                                v-model="email" 
+                                                type="email" 
+                                                name="email"
+                                                class="form-control form-hundi-input" placeholder="Email" value="">
+                                            <div :class="classes">{{ errors[0] }}</div>
+                                        </ValidationProvider>
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-lg-6">
                                     <div class="mb-3">
-                                        <input type="text" id="city" name="city" class="form-control form-hundi-input"
-                                            placeholder="City*" value="">
-                                        <div style="color:red;"></div>
+                                        <ValidationProvider v-slot="{ classes, errors }" rules="required|alpha_spaces" name="city">
+                                            <input 
+                                                id="city" 
+                                                v-model="city" 
+                                                type="text" 
+                                                name="city" 
+                                                class="form-control form-hundi-input"
+                                                placeholder="City*" value="">
+                                            <div :class="classes">{{ errors[0] }}</div>
+                                        </ValidationProvider>
                                     </div>
                                 </div>
                                 <div class="col-lg-6">
                                     <div class="mb-3">
-                                        <input type="text" id="state" name="state" class="form-control form-hundi-input"
-                                            placeholder="State*" value="">
-                                        <div style="color:red;"></div>
+                                        <ValidationProvider v-slot="{ classes, errors }" rules="required|alpha_spaces" name="state">
+                                            <input 
+                                                id="state" 
+                                                v-model="state" 
+                                                type="text" 
+                                                name="state" 
+                                                class="form-control form-hundi-input"
+                                                placeholder="State*" value="">
+                                            <div :class="classes">{{ errors[0] }}</div>
+                                        </ValidationProvider>
                                     </div>
                                 </div>
                             </div>
                             <div class="mb-3">
-                                <select name="trust" id="trust" class="form-control form-hundi-input">
+                                <select id="trust" v-model="trust" name="trust" class="form-control form-hundi-input">
                                     <!--<option value="null" >Select a-->
                                     <!--    Trust*</option>-->
                                     <!--<option value="Sai Mayee Trust"-->
                                     <!--    >Sai Mayee Trust-->
                                     <!--</option>-->
-                                    <option selected="selected" value="Sri Sai Meru Mathi Trust">Sri Sai
+                                    <option selected="selected" value="2">Sri Sai
                                         Meru Mathi Trust</option>
                                 </select>
                                 <div style="color:red;"></div>
                             </div>
-                            <div id="certification" class="mb-3" style="display: none;">
-                                <p><strong><span>Note:</span></strong><span id="certification_text"
-                                        style="color: #3c3489;"></span></p>
+                            <div 
+                            id="certification" 
+                            class="mb-3" 
+                            style="display: none;">
+                                <p>
+                                    <strong><span>Note:</span></strong>
+                                    <span 
+                                    id="certification_text"
+                                    style="color: #3c3489;"></span>
+                                </p>
                             </div>
-                            <div class="mb-3" id="pan_div" style="display: none;">
-                                <input type="text" id="pan" name="pan" class="form-control form-hundi-input"
+                            <div id="pan_div" class="mb-3" style="display: none;">
+                                <input 
+                                id="pan" 
+                                type="text" 
+                                name="pan" 
+                                class="form-control form-hundi-input"
                                     placeholder="PAN No.*" value="">
                                 <div style="color:red;"></div>
                             </div>
                             <div class="mb-3">
-                                <input type="text" id="amount" name="amount" class="form-control form-hundi-input"
-                                    placeholder="Amount*" value="">
-                                <div style="color:red;"></div>
-                            </div>
-                            <div class="mb-3">
-
-                                <div class="BDC_CaptchaDiv" id="Captcha_CaptchaDiv" style="width:330px; height:60px;">
-                                    <input type="hidden" name="BDC_VCID_Captcha" id="BDC_VCID_Captcha"
-                                        value="ccc103f1a91a1db210defcf7ce136239" />
-                                    <input type="hidden" name="BDC_BackWorkaround_Captcha"
-                                        id="BDC_BackWorkaround_Captcha" value="0" />
-                                    <input type="hidden" name="BDC_Hs_Captcha" id="BDC_Hs_Captcha"
-                                        value="6f29385858b5d3d62fc9a5f8ed508b689cda104f" />
-                                    <input type="hidden" name="BDC_SP_Captcha" id="BDC_SP_Captcha" value="213909019" />
-                                </div>
-                                <div style="color:red;"></div>
-                                <input id="captcha" type="text" name="captcha" value=""
-                                    class="form-control form-hundi-input  mt-3" required
-                                    placeholder="Enter the text from above image*" />
+                                    <ValidationProvider v-slot="{ classes, errors }" rules="required" name="amount">
+                                        <input 
+                                            id="amount" 
+                                            v-model="amount" 
+                                            type="text" 
+                                            name="amount" 
+                                            class="form-control form-hundi-input"
+                                            placeholder="Amount*" value="">
+                                        <div :class="classes">{{ errors[0] }}</div>
+                                    </ValidationProvider>
                             </div>
                             <div class="mb-3 form-check">
-                                <input type="checkbox" id="check" name="check"
-                                    class="form-check-input  form-hundi-checkbox">
-                                <label class="form-check-label" for="check">I accept the terms and conditions.</label>
-                                <div style="color:red;"></div>
+                                <ValidationProvider v-slot="{ classes, errors }" :rules="{ required: { allowFalse: false } }" name="terms and condition">
+                                    <input 
+                                        id="check"
+                                        v-model="accept" 
+                                        type="checkbox" 
+                                        class="form-check-input  form-hundi-checkbox" 
+                                        name="check">
+                                    <label class="form-check-label" for="check">I accept the <NuxtLink
+                                            to="/terms-condition" target="_blank">terms and
+                                            conditions</NuxtLink>.</label>
+                                    <div :class="classes">{{ errors[0] }}</div>
+                                </ValidationProvider>
                             </div>
                             <div class="mb-3 text-center">
                                 <button type="submit" class="btn btn-primary  form-hundi-submit">Make Payment</button>
                             </div>
                         </form>
+                        </ValidationObserver>
                     </div>
                 </div>
             </div>
@@ -122,6 +174,67 @@
 export default {
     name: "EHundiPage",
     layout: "MainPageLayout",
+    data() {
+        return {
+            first_name: '',
+            last_name: '',
+            email: '',
+            phone: '',
+            amount: '',
+            city: '',
+            state: '',
+            trust: '2',
+            accept: false,
+        }
+    },
+    methods: {
+        async formHandler() {
+            const loading = this.$loading({
+            lock: true,
+            fullscreen: true,
+            });
+            try {
+                const formData = new FormData;
+                formData.append('first_name', this.first_name);
+                formData.append('last_name', this.last_name);
+                formData.append('email', this.email);
+                formData.append('phone', this.phone);
+                formData.append('amount', this.amount);
+                formData.append('city', this.city);
+                formData.append('state', this.state);
+                formData.append('trust', this.trust);
+                const response = await this.$publicApi.post('/api/e-hundi/create', formData); // eslint-disable-line
+                this.$toast.success('Volunteer saved successfully')
+                this.first_name=''
+                this.last_name=''
+                this.email=''
+                this.phone=''
+                this.amount=''
+                this.city=''
+                this.state=''
+                this.trust=''
+                this.accept=false
+                this.$refs.form.reset();
+            } catch (err) {
+                // console.log(err.response);// eslint-disable-line
+                this.$refs.form.setErrors({
+                    first_name: err?.response?.data?.errors?.first_name,
+                    last_name: err?.response?.data?.errors?.last_name,
+                    email: err?.response?.data?.errors?.email,
+                    phone: err?.response?.data?.errors?.phone,
+                    amount: err?.response?.data?.errors?.amount,
+                    city: err?.response?.data?.errors?.city,
+                    state: err?.response?.data?.errors?.state,
+                    trust: err?.response?.data?.errors?.trust,
+                });
+                if(err?.response?.data?.message) this.$toast.error(err?.response?.data?.message)
+                if(err?.response?.data?.error) this.$toast.error(err?.response?.data?.error)
+                
+            }finally{
+            loading.close()
+            }
+        },
+    }
 }
 </script>
 
