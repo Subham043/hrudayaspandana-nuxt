ef385345223b98b5473c56c271f9708b0277c24b
JSPRO-11(Subham Saha) <js-pro-11@JSPRO-11s-MacBook-Pro.local>
2023-01-18 18:29:47 +0530

    complete

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,8 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  pageTransition: "fade",
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
